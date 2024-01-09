80658a8c86e79c4e40a617cbd6588534fa288446
drockparashar <pranshu007parashar@gmail.com>
2024-01-09 12:42:28 +0530

    Added project files

---
+++ src/CustomHooks/CurrencyConverter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
+function CurrencyConverter(currency){
+    const [data,setData]=useState({})
+    useEffect(()=>{
+        fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json")
+        .then((res)=>res.json())
+        .then((res)=>setData(res[currency]))
+    },[currency])
+
+    return data
+}
+
+export default CurrencyConverter
